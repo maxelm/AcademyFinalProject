9a2fcfcd4443c866add715620dc91f38002c46b5
ACADEMY-6303P31\Administrator <diar.marqus@gmail.com>
2017-09-14 13:52:30 +0200

    big changes

--- AcademyFinalProject/Models/ViewModels/AmountOfWorkVM.cs
+++ AcademyFinalProject/Models/ViewModels/AmountOfWorkVM.cs
@@ -13,37 +13,44 @@ namespace AcademyFinalProject.Models.ViewModels
         [Required(ErrorMessage = "Skriv in antal tim.")]
         [Display(Name = "Rivning")]
         public int DemolitionHours { get; set; }
+
         [Required(ErrorMessage = "Skriv in tim deb.")]
+        [DisplayFormat(DataFormatString = "{0:0}", ApplyFormatInEditMode = true)]
         public decimal HourlyRateDemolition { get; set; }
 
         [Required(ErrorMessage = "Skriv in antal tim.")]
         [Display(Name = "Avlopp")]
         public int DrainHours { get; set; }
         [Required(ErrorMessage = "Skriv in tim deb.")]
+        [DisplayFormat(DataFormatString = "{0:0}", ApplyFormatInEditMode = true)]
         public decimal HourlyRateDrain { get; set; }
 
         [Required(ErrorMessage = "Skriv in antal tim.")]
         [Display(Name = "Ventilation")]
         public int VentilationHours { get; set; }
         [Required(ErrorMessage = "Skriv in tim deb.")]
+        [DisplayFormat(DataFormatString = "{0:0}", ApplyFormatInEditMode = true)]
         public decimal HourlyRateVentilation { get; set; }
 
         [Required(ErrorMessage = "Skriv in antal tim.")]
         [Display(Name = "Kakel")]
         public int TileHours { get; set; }
         [Required(ErrorMessage = "Skriv in tim deb.")]
+        [DisplayFormat(DataFormatString = "{0:0}", ApplyFormatInEditMode = true)]
         public decimal HourlyRateTile { get; set; }
 
         [Required(ErrorMessage = "Skriv in antal tim.")]
         [Display(Name = "El")]
         public int ElectricityHours { get; set; }
         [Required(ErrorMessage = "Skriv in tim deb.")]
+        [DisplayFormat(DataFormatString = "{0:0}", ApplyFormatInEditMode = true)]
         public decimal HourlyRateElectricity { get; set; }
 
         [Required(ErrorMessage = "Skriv in antal tim")]
         [Display(Name = "Montering")]
         public int MountingHours { get; set; }
         [Required(ErrorMessage = "Skriv in tim deb.")]
+        [DisplayFormat(DataFormatString = "{0:0}", ApplyFormatInEditMode = true)]
         public decimal HourlyRateMounting { get; set; }
 
         [Display(Name = "Total tim:")]
@@ -52,9 +59,11 @@ namespace AcademyFinalProject.Models.ViewModels
         [Display(Name = "Total kostnad")]
         public decimal TotalWorkCost { get; set; }
 
+        [DisplayFormat(DataFormatString = "{0:0}", ApplyFormatInEditMode = true)]
         [Display(Name = "Resekostnad")]
         public decimal TravelCost { get; set; }
 
+        [DisplayFormat(DataFormatString = "{0:0}", ApplyFormatInEditMode = true)]
         [Display(Name = "Rabatt")]
         public decimal WorkDiscount { get; set; }
     }
